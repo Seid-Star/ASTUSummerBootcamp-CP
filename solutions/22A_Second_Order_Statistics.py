fad73a0f5a17a920c23a4bdd2e062ec248625aec
Seid-Star <sjrd02911@gmail.com>
2026-07-24 13:56:16 +0300

    Sync Codeforces: 22A - Second Order Statistics [PyPy 3-64]

--- solutions/22A_Second_Order_Statistics.py
+++ solutions/22A_Second_Order_Statistics.py
@@ -1,8 +1,21 @@
 a=int(input())
 arr=list(map(int,input().split()))
-brr=list(set(arr))
-brr.sort()
-if len(brr)==1:
+Min=10**9
+N_Min=10**9
+for i in arr:
+    if i<Min:
+        N_Min=Min
+        Min=i
+    elif Min<i<N_Min:
+        N_Min=i
+if N_Min==10**9:
     print("NO")
 else:
-    print(brr[1])
+    print(N_Min)
+        
+# brr=list(set(arr))
+# brr.sort()
+# if len(brr)==1:
+#     print("NO")
+# else:
+#     print(brr[1])
