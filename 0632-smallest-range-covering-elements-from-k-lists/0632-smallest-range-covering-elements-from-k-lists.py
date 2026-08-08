a089047a5e4d6ce1cdfa3178978df34a884460c0
Seid-Star <sjrd02911@gmail.com>
2026-08-08 13:59:57 +0300

    Time: 209 ms (98.25%), Space: 52.4 MB (5.13%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,37 +1,25 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        arr = []
-
+        arr=[]
         for i in range(len(nums)):
             for x in nums[i]:
-                arr.append((x, i))
-
+                arr.append((x,i))     
         arr.sort()
-
-        count = [0] * len(nums)
-        have = 0
-        left = 0
-        ans = [arr[0][0], arr[-1][0]]
-
-        for right in range(len(arr)):
-            value, group = arr[right]
-
-            if count[group] == 0:
-                have += 1
-
-            count[group] += 1
-
-            while have == len(nums):
-                lvalue, lgroup = arr[left]
-
-                if arr[right][0] - lvalue < ans[1] - ans[0]:
-                    ans = [lvalue, arr[right][0]]
-
-                count[lgroup] -= 1
-
-                if count[lgroup] == 0:
-                    have -= 1
-
-                left += 1
-
-        return ans
+        count=[0]*len(nums)
+        g=0
+        b=0
+        brr=[arr[0][0],arr[-1][0]]        
+        for a in range(len(arr)):
+            c,d=arr[a]           
+            if count[d]==0:
+                g+=1           
+            count[d]+=1          
+            while g==len(nums):
+                e,f=arr[b]              
+                if arr[a][0]-e<brr[1]-brr[0]:
+                    brr=[e,arr[a][0]]
+                count[f]-=1
+                if count[f]==0:
+                    g-=1
+                b+=1
+        return brr
