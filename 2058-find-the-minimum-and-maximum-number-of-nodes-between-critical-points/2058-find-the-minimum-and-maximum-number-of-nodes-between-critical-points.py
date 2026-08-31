d9f66871016b4a9a544b75990bde8e15b867d099
Seid-Star <sjrd02911@gmail.com>
2026-08-31 21:07:08 +0300

    Time: 70 ms (83.88%), Space: 62.9 MB (77.08%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+
+        prev = head
+        curr = head.next
+
+        index = 1
+
+        first = -1
+        last = -1
+        minDistance = float("inf")
+
+        while curr.next:
+
+            if ((curr.val > prev.val and curr.val > curr.next.val) or
+                (curr.val < prev.val and curr.val < curr.next.val)):
+
+                if first == -1:
+                    first = index
+
+                else:
+
+                    minDistance = min(minDistance, index - last)
+                last = index
+
+            prev = curr
+            curr = curr.next
+            index += 1
+        if first == last:
+            return [-1, -1]
+        maxDistance = last - first
+
+        return [minDistance, maxDistance]
